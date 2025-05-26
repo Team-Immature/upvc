93347d56d7eed3d6d8d78278171e4e7423a5674a
Bharath kumar <ipseudonerdi@gmail.com>
2025-05-27 01:54:36 +0530

    check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Test from "./components/Test";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <>
       <div>
         <h1>hello pvc --new change</h1>
+        <Test />
       </div>
     </>
   );
---
+++ frontend/src/components/Test.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = () => {
+  return <div>Test area check 1234</div>;
+};
+
+export default Test;
