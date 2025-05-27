6efef952a50781a3aaa69a3487821e7d8702280e
Santosh200474 <santoshpapani30@gmail.com>
2025-05-27 21:45:32 +0530

    New Navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Test from "./components/Test";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,8 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>hello pvc --new change</h1>
-        <Test />
+        <NavBar />
       </div>
     </>
   );
