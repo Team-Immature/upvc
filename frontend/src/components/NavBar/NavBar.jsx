42702db989edb6dece86bb010cb6072c05b9c604
Bharath kumar <ipseudonerdi@gmail.com>
2025-06-03 01:12:33 +0530

    last-slide-footer-header-fixes

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -7,14 +7,14 @@ const NavBar = () => {
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between px-4 py-3 bg-gradient-to-r from-gray-700 via-gray-600 to-gray-800">
-      <a className="text-2xl font-bold text-white uppercase" href="/">
+    <header className='sticky top-0 z-50 flex items-center justify-between px-4 py-3 bg-gradient-to-r from-gray-700 via-gray-600 to-gray-800'>
+      <a className='text-2xl font-bold text-white uppercase' href='/'>
         {/* <img class="w-[80px] h-[80px] rounded-full" src={logo} alt="" /> */}
         Logo
       </a>
 
       <button
-        className="text-white text-3xl md:hidden focus:outline-none"
+        className='text-white text-3xl md:hidden focus:outline-none'
         onClick={toggleMenu}
       >
         ☰
@@ -25,21 +25,33 @@ const NavBar = () => {
           isMenuOpen ? "flex" : "hidden"
         } absolute top-full left-0 w-full flex-col items-center bg-gray-800 md:static md:flex md:flex-row md:w-auto md:space-x-6 md:bg-transparent`}
       >
-        <a className="text-white py-2 px-4 hover:text-gray-300" href="/">
+        <a
+          className='text-white py-2 px-4 hover:text-gray-300 hover:scale-105 transition-all duration-150 mx-5'
+          href='/'
+        >
           Home
         </a>
-        <a className="text-white py-2 px-4 hover:text-gray-300" href="/products">
+        <a
+          className='text-white py-2 px-4 hover:text-gray-300 hover:scale-105 transition-all duration-150 mx-5'
+          href='/products'
+        >
           Products
         </a>
-        <a className="text-white py-2 px-4 hover:text-gray-300" href="/services">
+        <a
+          className='text-white py-2 px-4 hover:text-gray-300 hover:scale-105 transition-all duration-150 mx-5'
+          href='/services'
+        >
           Our Work
         </a>
-        <a className="text-white py-2 px-4 hover:text-gray-300" href="/contact">
+        <a
+          className='text-white py-2 px-4 hover:text-gray-300 hover:scale-105 transition-all duration-150 mx-5'
+          href='/contact'
+        >
           Contact Us
         </a>
       </nav>
 
-      <button className="hidden md:block px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-all duration-200">
+      <button className='hidden md:block px-5 py-2 bg-gray-200 text-gray-900  rounded-md hover:bg-black hover:text-white  transition-all hover:scale-108 duration-400'>
         Enquire Now
       </button>
     </header>
