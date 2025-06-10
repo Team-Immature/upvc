da17c77f0aeeff5e50752505ad16c4d0e65e9782
Bharath kumar <ipseudonerdi@gmail.com>
2025-06-10 13:09:23 +0530

    added scroll to top feature and fixed few bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './Home.jsx';
-import Contact from './Contact.jsx';
-import {Route ,Router ,BrowserRouter,Routes} from 'react-router-dom';
-import Products from './Products.jsx';
-import OurWork from './OurWork.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./Home.jsx";
+import Contact from "./Contact.jsx";
+import { Route, Router, BrowserRouter, Routes } from "react-router-dom";
+import Products from "./Products.jsx";
+import OurWork from "./OurWork.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 // import ScrollToTop from './components/scrollToTop.jsx';
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/products' element={<Products />} />
-      <Route path='/work' element={<OurWork />} />
-    </Routes>
-   
-    
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/work' element={<OurWork />} />
+      </Routes>
     </BrowserRouter>
     {/* <App /> */}
   </StrictMode>
-)
+);
