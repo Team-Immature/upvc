38f1baf11216589b005a96347ee2f34a1fcd1d57
Bharath kumar <ipseudonerdi@gmail.com>
2025-06-10 22:30:25 +0530

    fixed anchor tag inside link tag issue in nav-bar causing error in console-log

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import logo from "../../assets/r3.jpg";
 import { Link } from "react-router-dom";
 
-
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -14,7 +13,7 @@ const NavBar = () => {
     <header className='sticky top-0 z-50 flex items-center justify-between px-4 py-3 bg-gradient-to-r from-gray-700 via-gray-600 to-gray-800'>
       <div className='text-2xl font-bold text-white uppercase'>
         <img
-          class='w-[40px] h-[40px] hover:cursor-pointer rounded-lg'
+          className='w-[40px] h-[40px] hover:cursor-pointer rounded-lg'
           src={logo}
           alt=''
         />
@@ -32,39 +31,32 @@ const NavBar = () => {
           isMenuOpen ? "flex" : "hidden"
         } absolute top-full left-0 w-full flex-col items-center bg-gray-800 md:static md:flex md:flex-row md:w-auto md:space-x-6 md:bg-transparent`}
       >
-        <Link to="/">
-        <a
-          className='text-white py-2 px-4 hover:text-gray-300 hover:scale-105 cursor-pointer transition-all duration-150 mx-5'
-          
-          >
-          Home
-        </a>
+        <Link
+          to='/'
+          className='text-white py-2 px-4 hover:text-gray-300 hover:-translate-y-0.5   cursor-pointer transition-all duration-150 mx-5'
+        >
+          Home{" "}
         </Link>
-         <Link to="/products">
-        <a
-          className='text-white py-2 px-4 hover:text-gray-300 hover:scale-105 transition-all duration-150 mx-5'
-          
-          >
-          Products
-        </a>
-          </Link>
+        <Link
+          to='/products'
+          className='text-white py-2 px-4 hover:text-gray-300 hover:-translate-y-0.5   cursor-pointer transition-all duration-150 mx-5'
+        >
+          Products{" "}
+        </Link>
 
-          <Link to="/work">
-        <a
-          className='text-white py-2 px-4 hover:text-gray-300 hover:scale-105 transition-all duration-150 mx-5'
-          >
+        <Link
+          to='/work'
+          className='text-white py-2 px-4 hover:text-gray-300 hover:-translate-y-0.5   cursor-pointer transition-all duration-150 mx-5'
+        >
           Our Work
-        </a>
-          </Link>
+        </Link>
 
-          <Link to="/contact">
-        <a
-          className='text-white py-2 px-4 hover:text-gray-300 hover:scale-105 transition-all duration-150 mx-5'
-          
-          >
+        <Link
+          to='/contact'
+          className='text-white py-2 px-4 hover:text-gray-300 hover:-translate-y-0.5   cursor-pointer transition-all duration-150 mx-5'
+        >
           Contact Us
-        </a>
-          </Link>
+        </Link>
       </nav>
 
       <button className='hidden md:block px-5 py-2 bg-gray-200 text-gray-900  rounded-md hover:bg-black hover:text-white  transition-all hover:scale-108 duration-400'>
