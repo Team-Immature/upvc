eec5b9d64c40214046e68ca9f39f3af2c5f8bc2a
Santosh200474 <santoshpapani30@gmail.com>
2025-06-12 01:01:48 +0530

    Contact info updated

--- frontend/src/components/AddressDetailsLayout.jsx
+++ frontend/src/components/AddressDetailsLayout.jsx
@@ -13,12 +13,12 @@ const AddressDetailsLayout = () => {
           {/* phone number div */}
           <div className='flex flex-row  space-x-4 mt-10'>
             <FaPhone className='size-4 md:size-5 mr-2 my-auto' />
-            <h1 className='text-sm md:text-lg :'>+91203934038</h1>
+            <h1 className='text-sm md:text-lg :'>+91 8639685573</h1>
           </div>
           {/* email div */}
           <div className='flex flex-row space-x-4 mt-5'>
             <MdEmail className='size-4 md:size-5 my-auto mr-2' />
-            <h1 className='text-sm md:text-lg :'>zuzubee@gmail.com</h1>
+            <h1 className='text-sm md:text-lg :'>rsenterprise.upvc@gmail.com</h1>
           </div>
         </div>
         <div className='bg-[#f5f5f5] shadow-lg w-full h-1/2 rounded-2xl my-5  px-10 py-10 flex flex-col text-gray-700 hover:scale-102 transition-all duration-300 '>
@@ -29,7 +29,9 @@ const AddressDetailsLayout = () => {
           <div className='flex flex-row  space-x-4 mt-10'>
             <FaLocationDot className='size-8 md:size-5  my-auto' />
             <h1 className='text-sm md:text-lg :'>
-              +12711 W Tippecanoe Ranch Rd Delphi, Indiana(IN), 46923
+             R S Enterprises,UPVC WINDOWS AND DOORS ,
+
+            yellamma temple road, Narapally, no 6, Chowdhariguda, Hyderabad, Telangana 500088
             </h1>
           </div>
         </div>
@@ -41,7 +43,7 @@ const AddressDetailsLayout = () => {
       </div>
       <div className=' shadow-xl md:col-span-6 rounded-3xl h-[400px] md:h-2/3 my-auto'>
         <iframe
-          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.091099975581!2d78.66390277526736!3d17.45535328344355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb761a23802cc1%3A0x656b15a0c6919cb2!2sSNIST%20ADMIN%20BLOCK!5e0!3m2!1sen!2sin!4v1748720453126!5m2!1sen!2sin   '
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.1227516866516!2d78.64843877498568!3d17.405895783484432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9fef087cab59%3A0x7f359106611eecc5!2sR%20S%20Enterprises%2CUPVC%20WINDOWS%20AND%20DOORS!5e0!3m2!1sen!2sin!4v1749669945166!5m2!1sen!2sin"
           width='100%'
           height='100%'
           style={{ border: 0 }}
