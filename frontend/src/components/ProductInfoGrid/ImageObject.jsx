7103d4335d54c626da1ee75f9a4d01987ee66149
Santosh200474 <santoshpapani30@gmail.com>
2025-06-12 23:51:30 +0530

    series card images

---
+++ frontend/src/components/ProductInfoGrid/ImageObject.jsx
@@ -0,0 +1,53 @@
+import casementwindowframe from "../../assets/casementwindowframe.png";
+import casementoutswingwindowsash from "../../assets/casementoutswingwindowsash.png";
+import casementoutswingdoorsash from "../../assets/casementoutswingdoorsash.png";
+import casementmullion from "../../assets/casementmullion.png";
+import casementsingleglazing from "../../assets/casementsingleglazing.png";
+import smallwindowframe from "../../assets/smallwindowframe.png";
+import casementmeshsash from "../../assets/casementmeshsash.png";
+import casementslidingouterframe from "../../assets/casementslidingouterframe.png";
+
+export const sixtySeries = [   
+  { title: 'Casement Window Frame', image: casementwindowframe },
+  { title: 'Casement Outswing Window Sash', image: casementoutswingwindowsash },
+  { title: 'Casement Outswing Door Sash', image: casementoutswingdoorsash },
+  { title: 'Casement Mullion', image: casementmullion },
+  { title: 'Casement Single Glazing Bead', image: casementsingleglazing },
+  { title: '60 Small Window Frame', image: smallwindowframe },
+  { title: 'Casement Mesh Sash', image: casementmeshsash },
+  { title: 'Casement/Sliding Outer Frame', image: casementslidingouterframe },
+];
+
+import eightyouterframe from '../../assets/eightyouterframe.png';
+import eightyslidingwindowsash from '../../assets/eightyslidingwindowsash.png';
+import eightyslidingwindowmeshsash from '../../assets/80slidingwindowmeshsash.png';
+import eightyglazingbead from '../../assets/80glazingbead.png';
+import eightyinterlock from '../../assets/80interlock.png';
+import eightysashconnector from '../../assets/80smashconnector.png';
+import eightysashmullion from '../../assets/80sashmullion.png';
+import screensashmullion from '../../assets/screensashmullion.png';
+import screensashinterlock from '../../assets/screensashinterlock.png';
+import ninetynineouterframe from '../../assets/99outerframe.png';
+import eightyslidingdoorsash from '../../assets/80slidingdoorsash';
+import eightycurvedglazingbead from '../../assets/80curvedglazingbead.png';
+
+export const eightySeries = [   
+  { title: '80 Outer Frame', image: eightyouterframe },
+  { title: '80 Sliding Window Sash', image: eightyslidingwindowsash },
+  { title: '80 Sliding Window Mesh Sash', image: eightyslidingwindowmeshsash },
+  { title: '80 Glazing Bead', image: eightyglazingbead },
+  { title: '80 Interlock', image: eightyinterlock },
+  { title: '80 Sash Connector', image: eightysashconnector },
+  { title: '80 Sash Mullion', image: eightysashmullion },
+  { title: '99 Outer Frame', image: ninetynineouterframe },
+  { title: '80 Sliding Door Sash', image: eightyslidingdoorsash },
+  { title: '80 Curved Glazing Bead', image: eightycurvedglazingbead },
+];
+
+
+
+
+
+
+
+
