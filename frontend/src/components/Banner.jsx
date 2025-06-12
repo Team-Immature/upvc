147cf2a7b3ae4b112e5c0929b2321da8885e19b3
Bharath kumar <ipseudonerdi@gmail.com>
2025-06-12 23:31:46 +0530

    did minor fixes

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,13 +1,32 @@
 import React, { useEffect } from "react";
 import EnquiryForm from "./EnquiryForm";
 import { MdExpandCircleDown } from "react-icons/md";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Banner = (props) => {
   const loc = useLocation();
   const getLocation = () => {
     console.log(loc.pathname);
   };
+
+  const navigate = useNavigate();
+  const navigateTo = () => {
+    navigate("/#hero-section-top");
+  };
+
+  const location = useLocation();
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace("#", "");
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    }
+  }, [location]);
   return (
     <div className=''>
       <div className='relative w-full aspect-[16/9] max-h-[750px]  bg-[#b2b8b2] mb-20'>
@@ -17,7 +36,10 @@ const Banner = (props) => {
         />
         <EnquiryForm tcss='absolute top-1  right-10 z-20' />
         {loc.pathname == "/" && (
-          <MdExpandCircleDown className='hidden md:block absolute top-3/4 2xl:top-10/12 left-1/2 size-10 text-white animate-bounce rounded-full border-none' />
+          <MdExpandCircleDown
+            onClick={navigateTo}
+            className='hidden md:block absolute top-3/4 2xl:top-10/12 left-1/2 size-10 text-white animate-bounce rounded-full border-none hover:cursor-pointer'
+          />
         )}
       </div>
     </div>
